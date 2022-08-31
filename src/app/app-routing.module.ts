12be20edd0687bdfbc35061fd10fb922128f60ca
Gabriel <gabriel02ramos@gmail.com>
2022-08-31 11:01:42 -0300

    compreendendo rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//^as importações acima são obrigatórias
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: '', component: *}
+  //path:''--> é a representação em agular de '/'; 
+  //* qualquer componente já criado de ser declarado para ser uma rota EX: first-component
+  //lembre-se de importa cada componente que seja uma rota
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
